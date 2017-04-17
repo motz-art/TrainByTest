a1c1980141f6b41922db5b24f094e1f06a82a366
oleg <oshkoderov.oleg@gmail.com>
2017-04-17 17:02:03 +0200

    Implement value selection for VariantsQuestion.

--- src/Components/Test/Questions/VariantsQuestion/VariantsQuestion.js
+++ src/Components/Test/Questions/VariantsQuestion/VariantsQuestion.js
@@ -1,8 +1,28 @@
 import React, { Component } from 'react';
 
 class VariantsQuestion extends Component {
+  constructor(props) {
+    super(props);
+
+    this.updateValue = this.updateValue.bind(this);
+  }
+
+  updateValue(event) {
+    if (typeof this.props.handleValueUpdate !== 'function') {
+      throw new Error('Property handleValueUpdate is required and must be a function.');
+    }
+    this.props.handleValueUpdate(event.target.value);
+  }
+
   variants() {
-    return Array.isArray(this.props.questionVm.variants) && this.props.questionVm.variants.map((text, i) => (<div key={i} className="VariantsQuestion-Variant">{text}</div>));
+    return Array.isArray(this.props.questionVm.variants) && 
+      this.props.questionVm.variants.map((text, i) => (
+        <div key={i} className="VariantsQuestion-Variant">
+          <label>
+            <input type="radio" name="variant" value={text} checked={this.props.value===text} onChange={this.updateValue}  />{text}
+          </label>
+        </div>
+      ));
   }
   render() {
     return (
